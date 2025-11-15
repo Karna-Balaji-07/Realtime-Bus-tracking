e7dc010d458be1576283f1efadd3af8fab353add
Mahesh <118599861+Maheshdevada786@users.noreply.github.com>
2025-11-16 04:29:48 +0530

    new

---
+++ firebase.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB5TbdjcN6DkwsD9Xr34XJ2Nx6HWx2Q8i0",
+    authDomain: "bus-tracking-a0d37.firebaseapp.com",
+    databaseURL: "https://bus-tracking-a0d37-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "bus-tracking-a0d37",
+    storageBucket: "bus-tracking-a0d37.firebasestorage.app",
+    messagingSenderId: "1098696368650",
+    appId: "1:1098696368650:web:87d41cea634d40166b75f3"
+  };
+
+  // Initialize Firebase
+    const app = initializeApp(firebaseConfig);  
+    export default app;
